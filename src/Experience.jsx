39d0d506a1c3dea263d5fca828921cccb2be7a2d
Brad Babines <bbabines@gmail.com>
2023-06-11 15:22:30 -0400

    feat: Setup basic scene with model

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,17 +1,27 @@
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, Html, PresentationControls } from "@react-three/drei";
+
+import FemaleC from "./model-components/FemaleC";
 
 export default function Experience() {
 	return (
 		<>
-			<OrbitControls makeDefault />
+			{/* <OrbitControls makeDefault /> */}
 
 			<directionalLight position={[1, 2, 3]} intensity={1.5} />
 			<ambientLight intensity={0.5} />
 
-			<mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
-				<planeGeometry />
-				<meshStandardMaterial color="greenyellow" />
-			</mesh>
+			<Html transform distanceFactor={4} zIndexRange={[-10000, -10000]}>
+				<img src="/background.jpg" alt="Background picture" />
+			</Html>
+			<PresentationControls
+				polar={[0, 0]} // Vertical limits
+				azimuth={[-Infinity, Infinity]} // Horizontal limits
+				rotation={[0, 0.1, 0]}
+				snap={false} // Snap-back to center (can also be a spring config)
+				speed={3} // Speed factor
+			>
+				<FemaleC />
+			</PresentationControls>
 		</>
 	);
 }
