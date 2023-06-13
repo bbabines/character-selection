dbbd63711f15799253cffa7c79ddbe5d7525e10e
Brad Babines <bbabines@gmail.com>
2023-06-13 14:45:48 -0400

    feat: starting to better layout UI with flexbox and camera angle

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -12,7 +12,7 @@ export default function Experience() {
 
 	return (
 		<>
-			{/* <OrbitControls makeDefault /> */}
+			<OrbitControls makeDefault />
 			<directionalLight position={[1, 2, 3]} intensity={1.5} />
 			<ambientLight intensity={0.5} />
 
